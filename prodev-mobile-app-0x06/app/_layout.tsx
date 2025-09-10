8edb86d85f569fd3e0d4b1bb3f85e359e15157c7
lemmiEphraim <lemmiabdissa@gmail.com>
2025-09-10 13:29:03 +0300

    Update _layout.tsx

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,1 +1,11 @@
+import { Stack } from "expo-router";
+import "../styles/global.css";
 
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(home)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
